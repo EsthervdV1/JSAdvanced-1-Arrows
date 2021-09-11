06f920c904510381c7751d7e4edd5be05231558c
Esther <87823086+EsthervdV1@users.noreply.github.com>
2021-09-11 09:52:56 +0200

    Add files via upload

---
+++ arrows.js
@@ -0,0 +1,17 @@
+const ikRockArrowFunctions = function () {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+const ikRockArrowFunctions2 = () => console.log("Joe, ik rock de arrow functions!");
+
+const fivePlusSeven = function () {
+    return 5 + 7
+};
+
+const fivePlusSeven2 = () => { return 5 + 7 };
+
+let myFunction = () => { return a + b };
+
+let addFive = () => { return a + 5 };
+
+let createObject = () => { return "Greetings" };
